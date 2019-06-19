669a3ca96072a7dc5f49343373ac59a589c24224
Kenneth Skovhus <kenneth.skovhus@gmail.com>
2019-06-19 10:12:06 +0200

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-142422573-1`,
       },
     },
     `gatsby-plugin-feed`,
