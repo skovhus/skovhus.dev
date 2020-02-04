30b30d0668bd2838d52a4208e7de841a4fde0d3b
skovhus <kenneth.skovhus@gmail.com>
2020-02-04 14:07:23 +0100

    Setup emotion for styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,6 +74,7 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
