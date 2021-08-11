c4ce1faabf6f76cc47106a044b2afbc09eb73c14
skovhus <kenneth.skovhus@gmail.com>
2021-08-12 00:18:27 +0200

    Add target blank to external links

--- src/libs/markdown.ts
+++ src/libs/markdown.ts
@@ -3,19 +3,20 @@ import html from 'remark-html'
 import prism from 'remark-prism'
 import smartypants from '@silvenon/remark-smartypants'
 import figureCaption from 'gridsome-remark-figure-caption'
+import remarkExternalLinks from 'remark-external-links'
 
 export async function markdownToHtml(markdownString: string) {
   const result = await remark()
     .use(prism)
     .use(smartypants)
-    .use(html)
+    .use(remarkExternalLinks, {target: '_blank', rel: ['nofollow']})
     .use(figureCaption, {
       figureClassName: "figure-block",
       imageClassName: "figure-image",
       captionClassName: "figure-caption",
     })
+    .use(html)
     .process(markdownString)
 
-  // FIXME: gatsby-remark-external-links _target blank
   return result.toString()
 }
