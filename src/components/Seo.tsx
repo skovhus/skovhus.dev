2ffe44306bff7b561f0f0f4df4b2f9dde8cd634e
skovhus <kenneth.skovhus@gmail.com>
2021-08-12 22:02:47 +0200

    Replace react-helmet with nexy/head

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,94 +1,43 @@
 import React from 'react'
-import Helmet from 'react-helmet' // FIXME: migrate to next/head (https://github.com/leerob/gatsby-to-nextjs/pull/1/files)
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { siteMetadata } from '../config'
 
 type Props = {
   description?: string
   image?: string
-  meta: { name: string; content: string }[]
   pageTitle?: string
 }
 
-export default function SEO({ description, image, meta, pageTitle }: Props) {
+export default function SEO({ description, image, pageTitle }: Props) {
   const router = useRouter()
   const metaDescription = description || siteMetadata.description
 
   const { siteUrl, title: siteTitle } = siteMetadata
   const metaImageSrc = `${siteUrl}${image || '/skovhus.jpg'}`
   const url = `${siteUrl}${router.pathname || '/'}`
-
-  // FIMXE: test metaImageSrc
-
   const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang: 'en',
-      }}
-      title={title}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          name: `image`,
-          content: metaImageSrc,
-        },
-        {
-          property: 'og:url',
-          content: url,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          property: 'og:image',
-          content: metaImageSrc,
-        },
+    <Head>
+      <title>{title}</title>
+      <meta name="robots" content="follow, index" />
+      <meta name="description" content={metaDescription} />
+      <meta property="image" content={image} />
 
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:site`,
-          content: `@kenneth_skovhus`,
-        },
-        {
-          name: `twitter:creator`,
-          content: '@kenneth_skovhus',
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-        {
-          name: 'twitter:image',
-          content: metaImageSrc,
-        },
-      ].concat(meta)}
-    />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={metaImageSrc} />
+      <meta property="og:site_name" content={title} />
+
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:site" content="@kenneth_skovhus" />
+      <meta property="twitter:creator" content="@kenneth_skovhus" />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={metaDescription} />
+      <meta property="twitter:image" content={metaImageSrc} />
+    </Head>
   )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
 }
