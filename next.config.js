1d9ac72da7b23869d2b698cefc9ebb93d73ff405
skovhus <kenneth.skovhus@gmail.com>
2022-10-18 15:14:57 +0200

    Fix missing html lang

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer({})
+module.exports = withBundleAnalyzer({
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+})
