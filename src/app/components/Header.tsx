351d814a87bcfc9bad02bf0f9714a3da3ea86f24
skovhus <kenneth.skovhus@gmail.com>
2023-10-30 23:08:52 +0100

    Migrate header to css modules

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,88 +2,38 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
-import styled from 'styled-components'
 
+import styles from './Header.module.css'
 import ProfileImage from './ProfileImage'
 
 export function Header() {
   const pathname = usePathname()
 
-  const showBackButton = pathname?.includes('/blog/') || pathname === '/cv'
-
-  const navigation = (
-    <Nav>
-      <NavLink href="/" style={{ opacity: pathname == '/' ? 1 : 0.7 }}>
-        index
-      </NavLink>
-      {['blog', 'talks', 'music'].map((link) => (
-        <NavLink
-          href={`/${link}`}
-          style={{ opacity: pathname == `/${link}` ? 1 : 0.7 }}
-          key={link}
-        >
-          {link}
-        </NavLink>
-      ))}
-    </Nav>
-  )
-
   return (
-    <StyledHeader>
-      <StyledNav>
-        {showBackButton ? <BackButton /> : navigation}
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.navLinkContainer}>
+          <Link
+            href="/"
+            className={styles.navLink}
+            style={{ opacity: pathname == '/' ? 1 : 0.7 }}
+          >
+            index
+          </Link>
+          {['blog', 'talks', 'music'].map((link) => (
+            <Link
+              href={`/${link}`}
+              className={styles.navLink}
+              style={{ opacity: pathname == `/${link}` ? 1 : 0.7 }}
+              key={link}
+            >
+              {link}
+            </Link>
+          ))}
+        </div>
         <div style={{ flexGrow: 1 }} />
         <ProfileImage />
-      </StyledNav>
-    </StyledHeader>
+      </nav>
+    </header>
   )
 }
-
-const BackButton = () => (
-  <Link
-    href={'/'}
-    style={{
-      boxShadow: 'none',
-      textDecoration: 'none',
-      color: 'inherit',
-      display: 'flex',
-      alignItems: 'center',
-    }}
-  >
-    <BackArrow />
-  </Link>
-)
-
-const NavLink = styled(Link)`
-  text-decoration: none;
-
-  &:hover {
-    opacity: 1;
-  }
-`
-
-const StyledHeader = styled.header`
-  padding: 10px 0;
-`
-
-const StyledNav = styled.nav`
-  color: white;
-  display: flex;
-  align-items: center;
-  height: 80px;
-`
-
-const BackArrow = styled.div`
-  border: solid white;
-  border-width: 0 3px 3px 0;
-  display: inline-block;
-  padding: 6px;
-  transform: rotate(135deg);
-  margin-left: 10px;
-`
-
-const Nav = styled.div`
-  display: flex;
-  gap: 10px;
-  align-items: center;
-`
