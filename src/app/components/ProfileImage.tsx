bfccfdade7f512d170b8e95be15e4432652f2830
skovhus <kenneth.skovhus@gmail.com>
2023-10-30 23:36:39 +0100

    Migrate ProfileImage off styled components

--- src/app/components/ProfileImage.tsx
+++ src/app/components/ProfileImage.tsx
@@ -1,34 +1,28 @@
-'use client'
-import NextImage from 'next/legacy/image'
+import NextImage from 'next/image'
 import React from 'react'
-import styled from 'styled-components'
 
 export const PROFILE_IMAGE_WIDTH = 50
 
-const ImageContainer = styled.div`
-  margin-bottom: 0;
-  width: ${PROFILE_IMAGE_WIDTH}px;
-  height: ${PROFILE_IMAGE_WIDTH}px;
-  position: relative;
-  flex-shrink: 0;
-`
-
-const RoundedImage = styled(NextImage)`
-  border-radius: 100%;
-`
-
 export default function ProfileImage() {
   return (
-    <ImageContainer>
+    <div
+      style={{
+        marginBottom: 0,
+        width: PROFILE_IMAGE_WIDTH,
+        height: PROFILE_IMAGE_WIDTH,
+        position: 'relative',
+        flexShrink: 0,
+      }}
+    >
       {/* Type instantiation is excessively deep and possibly infinite.
        // @ts-ignore*/}
-      <RoundedImage
+      <NextImage
         src="/skovhus.jpg"
         alt={'Kenneth Skovhus'}
-        layout="fixed"
         width={PROFILE_IMAGE_WIDTH}
         height={PROFILE_IMAGE_WIDTH}
+        style={{ borderRadius: '100%' }}
       />
-    </ImageContainer>
+    </div>
   )
 }
