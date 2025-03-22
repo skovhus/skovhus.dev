2149c3b8a286d5381c0d177c521f0c1a0c8a96de
skovhus <kenneth.skovhus@gmail.com>
2025-03-22 16:07:48 +0100

    Wording

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,16 @@ export default function Home() {
   return (
     <>
       <HugeHeading>
-        Hey, I&apos;m Kenneth. I&apos;m a software engineer, 2 x dad, and music nerd.
+        Hey, I&apos;m Kenneth. I&apos;m a software engineer, 2x dad, and music nerd.
       </HugeHeading>
       <p style={{ marginTop: '2rem' }}>
-        Based in Copenhagen, Denmark. Writes code for{' '}
+        Based in Copenhagen, Denmark. Currently crafting code at{' '}
         <ExternalLink href="https://linear.app/">Linear</ExternalLink>.
       </p>
 
       <p>
-        This is my portfolio where I gather random bits and pieces – my attempt to fight
-        digital sharecropping and distilling knowledge.
+        This is my portfolio where I gather random bits – my attempt to fight digital
+        sharecropping and distill learnings.
       </p>
       <p>
         Follow via <Link href="/feed">RSS</Link> or connect on{' '}
